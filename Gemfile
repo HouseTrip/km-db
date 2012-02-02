7ac1871a2f4e91973d258b01849c5b31d239c325
Julien Letessier <jtl@housetrip.com>
2012-02-02 14:19:47 +0000

    Fixed Event.with_properties and Event.named. Removed Event.with_property (redundant). Added Event.by_date.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source :gemcutter
 
 gem 'progressbar', :git => 'git@github.com:HouseTrip/progressbar.git'
+
 # Specify your gem's dependencies in km.gemspec
 gemspec
--- lib/km/db/event.rb
+++ lib/km/db/event.rb
@@ -7,13 +7,7 @@ module KM::DB
 
     set_table_name "events"
     has_many   :properties, :class_name => 'KM::DB::Property'
-    named_scope :with_property, lambda { |prop|
-      prop_table = Property.table_name
-      { 
-        :select => "`#{table_name}`.*, `#{prop_table}`.`value` AS `#{prop}`",
-        :joins => sanitize_sql_array(["INNER JOIN `properties` ON `#{table_name}`.id = `#{prop_table}`.event_id AND `properties`.`key` = ?", prop])
-      }
-    }
+
     named_scope :with_properties, lambda { |*props|
       prop_table = Property.table_name
       selects = ["`#{table_name}`.*"]
@@ -24,14 +18,16 @@ module KM::DB
         joins << sanitize_sql_array([%Q{
           INNER JOIN `properties` AS `#{temp_name}`
           ON `#{table_name}`.id = `#{temp_name}`.event_id 
-          AND `#{temp_name}`.`key` = ?}, prop])
+          AND `#{temp_name}`.`key` = ?}, KM::DB::Key.get(prop)])
       }
       { :select => selects.join(', '), :joins => joins.join("\n") }
     }
     named_scope :before, lambda { |date| { :conditions => ["`t` < ?", date] } }
     named_scope :after,  lambda { |date| { :conditions => ["`t` > ?", date] } }
 
-    named_scope :named, lambda { |name| { :conditions => { :n => name } } }
+    named_scope :named, lambda { |name| { :conditions => { :n => KM::DB::Key.get(name) } } }
+
+    named_scope :by_date, :order => '`t` ASC'
 
     # return value of property
     def prop(name)
