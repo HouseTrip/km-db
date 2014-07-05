1cc8f4af186d450b160d73780e73cebcaaaa0a62
Julien Letessier <julien.letessier@gmail.com>
2014-07-05 20:25:26 +0200

    Cleans up toplevel module

--- lib/kmdb.rb
+++ lib/kmdb.rb
@@ -4,20 +4,23 @@ module KMDB
   DEFAULT_DB_URL = 'sqlite3:test.db'
   MIGRATIONS_DIR = Pathname(__FILE__).parent.join('kmdb/migrations').cleanpath.to_s
 
-  def self.env
-    ENV['RACK_ENV'] || ENV['KMDB_ENV'] || 'development'
-  end
+  module ModuleMethods
+    def env
+      ENV['RACK_ENV'] || ENV['KMDB_ENV'] || 'development'
+    end
 
-  def self.connect
-    url = ENV.fetch('DATABASE_URL', DEFAULT_DB_URL)
-    puts url
-    ActiveRecord::Base.establish_connection(url)
-    self
-  end
+    def connect
+      url = ENV.fetch('DATABASE_URL', DEFAULT_DB_URL)
+      puts url
+      ActiveRecord::Base.establish_connection(url)
+      self
+    end
 
-  def self.migrate
-    ActiveRecord::Migration.verbose = true
-    ActiveRecord::Migrator.migrate MIGRATIONS_DIR
-    self
+    def migrate
+      ActiveRecord::Migration.verbose = true
+      ActiveRecord::Migrator.migrate MIGRATIONS_DIR
+      self
+    end
   end
+  extend ModuleMethods
 end
