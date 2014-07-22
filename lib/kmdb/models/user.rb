8c172e0c34827d5bb49a5dd2ca8dd0e66422e984
Julien Letessier <julien.letessier@gmail.com>
2014-07-22 16:05:23 +0200

    Adds User object cache

--- lib/kmdb/models/user.rb
+++ lib/kmdb/models/user.rb
@@ -1,5 +1,6 @@
 require 'kmdb/models/custom_record'
 require 'kmdb/concerns/has_properties'
+require 'kmdb/redis'
 
 module KMDB
   class User < ActiveRecord::Base
@@ -17,6 +18,33 @@ module KMDB
     # return (latest) value of property
     def prop(name)
       properties.named(name).first.andand.value
+    end
+
+    CACHE_EXPIRY = 3_600
+
+    def self.find_or_create(name:)
+      if raw = _redis.get(name)
+        Marshal.load(raw)
+      else
+        super.tap do |user|
+          _redis.set(name, Marshal.dump(user), ex: CACHE_EXPIRY)
+        end
+      end
+    end
+
+    def destroy
+      super
+      _redis.del(name)
+    end
+
+    private
+
+    def _redis(*args)
+      self.class._redis(*args)
+    end
+
+    def self._redis
+      @_redis ||= Redis.namespaced('kmdb:users')
     end
   end
 end
