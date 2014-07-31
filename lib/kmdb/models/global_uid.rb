8eefb0c1e8edcebdf6fb36890c3e45d9dd7b5b58
Julien Letessier <julien.letessier@gmail.com>
2014-07-31 11:20:32 +0200

    Makes GlobalUID much more efficient

--- lib/kmdb/models/global_uid.rb
+++ lib/kmdb/models/global_uid.rb
@@ -2,19 +2,39 @@ require 'kmdb'
 require 'kmdb/redis'
 
 module KMDB
-  module GlobalUID
-    module ModuleMethods
+  # Efficiently generate cross-process globally unique IDs
+  # pernamespace, using Redis.
+  class GlobalUID
 
-      def get(ns = 'value')
-        _redis.incr(ns) % (1 << 48)
-      end
-
-      private
-
-      def _redis
-        @_redis ||= Redis.namespaced('kmdb:globaluid')
-      end
+    def self.get(ns = 'value')
+      @instances ||= {}
+      @instances[ns] ||= new(ns)
+      @instances[ns].get
     end
-    extend ModuleMethods
+
+    def initialize(ns)
+      @ns = ns
+      @major = nil
+      @minor = nil
+    end
+
+    def get
+      if @major.nil? || @minor >= BATCH_SIZE
+        @major = _redis.incr(@ns) % (1 << 48)
+        @minor = 0
+      end
+
+      uid = (@major - 1) * BATCH_SIZE + @minor
+      @minor += 1
+      return uid
+    end
+
+    private
+
+    BATCH_SIZE = 100
+
+    def _redis
+      @@_redis ||= Redis.namespaced('kmdb:globaluid:v2')
+    end
   end
 end
