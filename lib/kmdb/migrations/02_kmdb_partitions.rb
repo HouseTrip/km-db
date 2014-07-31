575c37ddc40603f5d9882f5979207638b93df139
Julien Letessier <julien.letessier@gmail.com>
2014-07-31 12:34:46 +0200

    Changes event/property ID columns to BIGINT

--- lib/kmdb/migrations/02_kmdb_partitions.rb
+++ lib/kmdb/migrations/02_kmdb_partitions.rb
@@ -11,11 +11,11 @@ class KmdbPartitions < ActiveRecord::Migration
   def up
     %w(events properties aliases).each do |t|
       sql = %Q{
-        ALTER TABLE #{t} CHANGE id id INT(11) NOT NULL
+        ALTER TABLE #{t} CHANGE id id BIGINT NOT NULL
         ALTER TABLE #{t} DROP PRIMARY KEY
         CREATE UNIQUE INDEX index_events_partition ON #{t} (t, id)
         CREATE INDEX index_events_id ON #{t} (id)
-        ALTER TABLE #{t} CHANGE id id INT(11) NOT NULL AUTO_INCREMENT
+        ALTER TABLE #{t} CHANGE id id BIGINT NOT NULL AUTO_INCREMENT
         ALTER TABLE #{t} PARTITION BY RANGE COLUMNS (t) (PARTITION pLast VALUES LESS THAN MAXVALUE)
       }
       sql.strip.split(/\n/).each { |stmt| execute stmt.strip }
